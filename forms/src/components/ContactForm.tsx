01422ace303373e4e90cf05941b898529aa6ec66
cappscr <capps.christopher@gmail.com>
2026-01-04 05:52:36 -0500

    Refactors form status to utilize the useActionStatus hook instead of useFormStatus

--- forms/src/components/ContactForm.tsx
+++ forms/src/components/ContactForm.tsx
@@ -1,21 +1,36 @@
 'use client';
-import { useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
 import { insertContact } from '@/data/insertContact';
 
 export function ContactForm() {
+  const [{ ok, error, formData }, formAction, isPending] = useActionState(insertContact, {
+    ok: false,
+    error: '',
+    formData: new FormData(),
+  });
   return (
-    <form action={insertContact}>
+    <form action={formAction}>
       <div className="field">
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          defaultValue={(formData.get('name') ?? '') as string}
+        />
       </div>
       <div className="field">
         <label htmlFor="email">Your email address</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          defaultValue={(formData.get('email') ?? '') as string}
+        />
       </div>
       <div className="field">
         <label htmlFor="reason">Reason you need to contact us</label>
-        <select id="reason" name="reason">
+        <select id="reason" name="reason" defaultValue={(formData.get('reason') ?? '') as string}>
           <option value=""></option>
           <option value="Support">Support</option>
           <option value="Feedback">Feedback</option>
@@ -24,21 +39,17 @@ export function ContactForm() {
       </div>
       <div className="field">
         <label htmlFor="notes">Additional notes</label>
-        <textarea id="notes" name="notes" />
+        <textarea id="notes" name="notes" defaultValue={(formData.get('notes') ?? '') as string} />
       </div>
-      <SubmitButton />
-    </form>
-  );
-}
-
-function SubmitButton() {
-  const { pending } = useFormStatus();
-  return (
-    <>
-      {pending && <p role="alert">Saving ...</p>}
-      <button type="submit" disabled={pending}>
+      {!ok && (
+        <p role="alert" className="error">
+          {error}
+        </p>
+      )}
+      {isPending && <p role="alert">Saving ...</p>}
+      <button type="submit" disabled={isPending}>
         Submit
       </button>
-    </>
+    </form>
   );
 }
--- forms/src/data/insertContact.ts
+++ forms/src/data/insertContact.ts
@@ -4,15 +4,26 @@ import { createClient, type Client } from '@libsql/client';
 import { redirect } from 'next/navigation';
 import { contactSchema } from './schema';
 
-export async function insertContact(formData: FormData) {
+type ActionState = {
+  ok: boolean;
+  error: string;
+  formData: FormData;
+};
+
+export async function insertContact(previousState: ActionState, formData: FormData) {
   const parsedResult = contactSchema.safeParse(Object.fromEntries(formData));
   if (!parsedResult.success) {
-    return;
+    return {
+      ok: false,
+      error: 'Unable to save - invalid field values',
+      formData,
+    };
   }
   const { name, email, reason, notes } = parsedResult.data;
 
   let client: Client | undefined;
   let ok = true;
+  let error = '';
 
   try {
     client = createClient({
@@ -24,6 +35,7 @@ export async function insertContact(formData: FormData) {
     });
   } catch {
     ok = false;
+    error = 'Problem saving form';
   }
   if (client) {
     client.close();
@@ -31,4 +43,5 @@ export async function insertContact(formData: FormData) {
   if (ok) {
     redirect(`/thanks?name=${encodeURIComponent(name)}`);
   }
+  return { ok, error, formData };
 }
